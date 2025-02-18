931ad11af2f14c52b8fb5b6c3585a22f79f0c707
njaberi <nezarajaberi@gmail.com>
2025-02-18 21:02:40 +0300

    push

---
+++ build/precache-manifest.d3d7ee7a8af8f5dc92fdac9af2b16a04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2816648f69d7e55d0ef9d1abd02b89c1",
+    "url": "/graphql/index.html"
+  },
+  {
+    "revision": "5ceb0199afb5efa16a3f",
+    "url": "/graphql/static/css/main.d856fabd.chunk.css"
+  },
+  {
+    "revision": "f28afa7f5d5d19c3ce7d",
+    "url": "/graphql/static/js/2.f2c17f44.chunk.js"
+  },
+  {
+    "revision": "d4b4259a290c788aec291821574a2b0b",
+    "url": "/graphql/static/js/2.f2c17f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b8900fbe151be342b7",
+    "url": "/graphql/static/js/3.4d230220.chunk.js"
+  },
+  {
+    "revision": "5ceb0199afb5efa16a3f",
+    "url": "/graphql/static/js/main.5d1d75f6.chunk.js"
+  },
+  {
+    "revision": "c23d7af56886d1f14eaa",
+    "url": "/graphql/static/js/runtime-main.75c3c15a.js"
+  }
+]);
